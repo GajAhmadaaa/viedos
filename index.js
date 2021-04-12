cb834f155e455269ae63c5ef0b06014d96b71e1e
GajAhmadaaa <64576446+GajAhmadaaa@users.noreply.github.com>
2021-04-12 13:40:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
+const PORT = parseInt(process.env.PORT) || 3000;
 app.use('/static', express.static('./static'));
 app.use('/assets', express.static('./assets'));
 
-app.listen(3000, () => { 
-    console.log("It Works!");
+app.listen(PORT, () => { 
+    console.log("> Listining on http://localhost:${PORT}");
 });
 
 app.get('/', (req, res) => { 
